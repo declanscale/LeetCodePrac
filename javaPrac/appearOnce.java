a30bdab257af3b4c9de1fca05e6d2a0ccebdd842
daisong yu <declanscale@gmail.com>
2019-10-25 10:47:17 +0800

    comments

--- javaPrac/appearOnce.java
+++ javaPrac/appearOnce.java
@@ -1,6 +1,8 @@
 import java.util.*;
-//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
-
+/*
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次,
+找出那个只出现了一次的元素。
+*/
 public class appearOnce{
 	public static int singleNumber(int[] nums) {
         
