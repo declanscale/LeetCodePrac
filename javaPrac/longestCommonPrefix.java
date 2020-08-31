c266c81993b04bb2baafbf3283ac31bcc1aedf11
daisong yu <declanscale@gmail.com>
2020-08-31 21:34:46 +0800

    add comments to longestCommonPrefix

--- javaPrac/longestCommonPrefix.java
+++ javaPrac/longestCommonPrefix.java
@@ -17,8 +17,8 @@ class longestCommonPrefix{
 
             for(int j = 1; j <  strs.length; j++){
                 if(strs[j].length() == i || strs[j].charAt(i) != c ){
+                	//这个条件判断 ||两边的顺序会影响运行的结果， 会先判断左边的 再判断右边的
                     return strs[0].substring(0,i);
-                    
                 }
             }
         }
